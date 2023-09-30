b256da7b5ecab823143021576690de86bf57fcf9
KHANH LE NGUYEN <132900864+nkhanhle23@users.noreply.github.com>
2023-09-30 18:10:47 +0200

    extracted EBIT file

--- extract_metrics.py
+++ extract_metrics.py
@@ -2,11 +2,12 @@ import requests
 import pandas as pd
 from tqdm import tqdm
 import json
+import os  # Import the os module to manage files
 
 # Define metrics
-metrics = ['NetIncomeLoss','RevenueFromContractWithCustomerExcludingAssessedTax',
+metrics = ['NetIncomeLoss', 'RevenueFromContractWithCustomerExcludingAssessedTax',
            'EarningsPerShareDiluted', 'NetCashProvidedByUsedInContinuingOperations',
-           'NetCashProvidedByUsedInFinancingActivities', 
+           'NetCashProvidedByUsedInFinancingActivities',
            'NetCashProvidedByUsedInInvestingActivities',
            'CostOfGoodsAndServicesSold', 'SellingGeneralAndAdministrativeExpense'
            ]
@@ -18,11 +19,12 @@ end_year = 2022
 base_url = "https://data.sec.gov/api/xbrl/frames/us-gaap"
 header = {'Accept': "application/json", 'User-Agent': "s_nguyenk22@stud.hwr-berlin.de"}
 
-# Create an empty list to store the results
-all_data = []
+# Create a directory to store the metric-specific files
+output_directory = 'datasets/metric_data'
+os.makedirs(output_directory, exist_ok=True)
 
 # Loop through metrics and years
-for metric in metrics: 
+for metric in metrics:
     metric_data = []  # Create a list for data related to the current metric
     for year in tqdm(range(start_year, end_year + 1), desc=f"Fetching {metric}"):
         url = f"{base_url}/{metric}/USD/CY{year}.json"
@@ -30,7 +32,7 @@ for metric in metrics:
             data = requests.get(url, headers=header).json()
             df = pd.DataFrame(data['data'])
             df['year'] = year
-            
+
             df['metric'] = metric
 
             metric_data.append(df)
@@ -39,27 +41,25 @@ for metric in metrics:
 
     # Concatenate all the dataframes related to the current metric
     if metric_data:
-        all_data.extend(metric_data)
+        final_df = pd.concat(metric_data, ignore_index=True)
 
-# Concatenate all the dataframes into one
-final_df = pd.concat(all_data, ignore_index=True)
+        # Rename the columns
+        final_df['metric'] = final_df['metric'].replace({
+            'NetIncomeLoss': 'Net Income (Loss)',
+            'RevenueFromContractWithCustomerExcludingAssessedTax': 'Revenue',
+            'EarningsPerShareDiluted': 'Diluted Earnings per share',
+            'NetCashProvidedByUsedInContinuingOperations': 'Net Cash from Operating Activities',
+            'NetCashProvidedByUsedInFinancingActivities': 'Net Cash from Financing Activities',
+            'NetCashProvidedByUsedInInvestingActivities': 'Net Cash from Investing Activities',
+            'CostOfGoodsAndServicesSold': 'Cost of Goods Sold',
+            'SellingGeneralAndAdministrativeExpense': 'Selling, General and Administrative Expenses'
+        })
 
-# Rename the columns
-final_df['metric'] = final_df['metric'].replace({
-    'NetIncomeLoss': 'Net Income (Loss)',
-    'RevenueFromContractWithCustomerExcludingAssessedTax': 'Revenue',
-    'EarningsPerShareDiluted': 'Diluted Earnings per share',
-    'NetCashProvidedByUsedInContinuingOperations': 'Net Cash from Operating Activities',
-    'NetCashProvidedByUsedInFinancingActivities': 'Net Cash from Financing Activities',
-    'NetCashProvidedByUsedInInvestingActivities': 'Net Cash from Investing Activities', 
-    'CostOfGoodsAndServicesSold': 'Cost of Goods Sold',
-    'SellingGeneralAndAdministrativeExpense': 'Selling, General and Administrative Expenses'
-})
+        # Define the output file path for the current metric
+        output_file = os.path.join(output_directory, f'{metric}.csv')
 
-
-
-# Save the concatenated dataframe to a CSV file
-final_df.to_csv('datasets/financial_data.csv', index=False)
+        # Save the current metric's data to a CSV file
+        final_df.to_csv(output_file, index=False)
 
 # Print a completion message
 print("Done")
